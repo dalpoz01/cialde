fbe4ca1050a9d2d992c8865ebd33d716a6403931
giuliozanatta <giuliozanatta98@gmail.com>
2021-05-06 15:50:29 +0200

    Revert "Add model"
    This reverts commit 14186ccb7684e415aa42b040b798265a6f2256d0.

--- model/model.cpp
+++
@@ -1,9 +0,0 @@
-#include "model.h"
-
-Model::Model(u_int n) : container(n) {}
-
-void Model::addBox(WaffleBox* waffleb){container.push_back(DeepPtr<WaffleBox>(waffleb));}
-
-void Model::removeBox(u_int index){container.erase(index);}
-
-void Model::removeBox(DeepPtr<WaffleBox>& ptr){container.erase(ptr);}
