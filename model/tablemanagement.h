e659fc604d4e62e132ab2ec5c9c0ea46f2295eea
giuliozanatta <giuliozanatta98@gmail.com>
2021-05-08 13:03:03 +0200

    Aggiunti commenti su tableManagement

--- model/tablemanagement.h
+++ model/tablemanagement.h
@@ -11,12 +11,12 @@ public:
     TableManagement(QObject * = nullptr,const std::string& = "../cialde-test/Data/Xml/", const std::string& = "product.xml");
     ~TableManagement();
 
-    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex& = QModelIndex()) const override;
-    QVariant data(const QModelIndex&, int = Qt::DisplayRole) const override;
-    QVariant headerData(int, Qt::Orientation, int) const override;
-    bool insertRows(int, int, const QModelIndex&);
-    bool removeRows(int, int, const QModelIndex&);
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override; //Metodo di QAbstractTableModel che ritorna numero di righe per la view
+    int columnCount(const QModelIndex& = QModelIndex()) const override; //Metodo di QAbstractTableModel che ritorna numero di colonne per la view
+    QVariant data(const QModelIndex&, int = Qt::DisplayRole) const override; //Metodo di QAbstractTableModel che serve per rappresentare i dati nella cella
+    QVariant headerData(int, Qt::Orientation, int) const override;  //Metodo di QAbstractTableModel che inserisce i giusti nomi nelle colonne della tabella
+    bool insertRows(int, int, const QModelIndex&);  //Metodo di QAbstractTableModel che inserisce una riga sulla tabella
+    bool removeRows(int, int, const QModelIndex&); //Metodo di QAbstractTableModel che rimuove una riga dalla tabella
 };
 
 #endif // TABLEMANAGEMENT_H
